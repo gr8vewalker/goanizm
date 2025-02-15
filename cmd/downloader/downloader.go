6a028903421d4c95ff870397452c3cfa185f1952
gr8vewalker <ahmetflix@duck.com>
2025-02-15 17:45:16 +0300

    feat: episode selection

--- cmd/downloader/downloader.go
+++ cmd/downloader/downloader.go
@@ -1,43 +1,95 @@
 package main
 
 import (
-	"bufio"
-	"fmt"
 	"log"
-	"os"
+	"slices"
+	"strconv"
 	"strings"
 
 	"github.com/fatih/color"
+	"github.com/gr8vewalker/goanizm/internal/cli"
 	"github.com/gr8vewalker/goanizm/internal/parser"
 )
 
-func readLine() string {
-	in := bufio.NewReader(os.Stdin)
-	line, err := in.ReadString('\n')
-	if err != nil {
-		log.Fatalln("Cannot read line from input", err)
-	}
-	return strings.TrimSpace(line)
-}
-
 func main() {
+	// --- SEARCHING ---
 	color.Cyan("What do you want to download?")
-	query := readLine()
+	query := cli.ReadLine()
 	results, err := parser.Search(query)
 	if err != nil {
 		log.Fatalln("Cannot done a search", err)
 	}
 
+	// --- ANIME SELECTION ---
 	color.Cyan("Select an anime: ")
 	for index, result := range results {
-		color.Magenta("%v - %v\n", index, result.Name)
+		color.Magenta("%v - %v\n", index + 1, result.Name)
 	}
 
-	var selectionIndex uint
-	fmt.Scanln(&selectionIndex)
+	selectionIndex, err := cli.ReadIntegerFiltered(func(i int) bool {
+		return !(i < 1 || i > len(results))
+	})
 
-	selected := results[selectionIndex]
+	if err != nil {
+		log.Fatalln("Cannot do selection", err)
+	}
 
+	selected := results[selectionIndex - 1]
+
+	// --- DETAILS OF SELECTED ANIME ---
 	anime, err := parser.Details(selected)
 	color.Cyan("Selected anime: %v", color.MagentaString(anime.Name))
+
+	// --- EPISODE SELECTION ---
+	selectedEpisodes := selectEpisodes(anime.Episodes)
+	color.Red("Selected %v episodes", len(selectedEpisodes))
+}
+
+func selectEpisodes(episodes []parser.Episode) []int {
+	color.Cyan("Please select episode(s) to download.")
+	color.Cyan("If you finished selecting enter 'yes'")
+
+	var selectedEpisodes []int
+
+	printEpisodeSelector := func(initialRun bool) {
+		if !initialRun {
+			cli.MoveUp(len(episodes) + 1)
+			cli.CarriageReturn()
+		}
+		for index, episode := range episodes {
+			status := " "
+			if slices.Contains(selectedEpisodes, index + 1) {
+				status = "x"
+			}
+			color.Magenta("[%v] %v - %v", status, index + 1, episode.Name)
+		}
+	}
+
+	printEpisodeSelector(true)
+
+	for {
+		input := cli.ReadLine()
+
+		if strings.EqualFold(input, "yes") {
+			break
+		}
+
+		i, err := strconv.Atoi(input)
+
+		if err != nil || i < 1 || i > len(episodes) {
+			printEpisodeSelector(false)
+			continue
+		}
+
+		if slices.Contains(selectedEpisodes, i) {
+			index := slices.Index(selectedEpisodes, i)
+			selectedEpisodes = slices.Delete(selectedEpisodes, index, index+1)
+		} else {
+			selectedEpisodes = append(selectedEpisodes, i)
+		}
+
+		printEpisodeSelector(false)
+	}
+
+	return selectedEpisodes
 }
---
+++ internal/cli/cli.go
@@ -0,0 +1,53 @@
+package cli
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func MoveUp(n int) {
+	fmt.Printf("\033[%dA", n)
+}
+
+func ClearLine() {
+	fmt.Print("\033[2K\r")
+}
+
+func CarriageReturn() {
+	fmt.Print("\r")
+}
+
+func ReadInteger() (int, error) {
+	return ReadIntegerFiltered(nil)
+}
+
+func ReadIntegerFiltered(filter func(int) bool) (int, error) {
+	str := ReadLine()
+
+	var read int
+
+	for {
+		i, err := strconv.Atoi(str)
+
+		if err != nil || (filter != nil && !filter(i)) { // try until a good integer input comes.
+			continue
+		}
+		read = i
+		break
+	}
+
+	return read, nil
+}
+
+func ReadLine() string {
+	in := bufio.NewReader(os.Stdin)
+	line, err := in.ReadString('\n')
+	if err != nil {
+		log.Fatalln("Cannot read line from input", err)
+	}
+	return strings.TrimSpace(line)
+}
